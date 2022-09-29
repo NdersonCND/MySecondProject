ef94bdabb115c9ffa031da549ed6ad31ff3ce0e3
NdersonCND <112896539+NdersonCND@users.noreply.github.com>
2022-09-28 20:37:15 -0600

    Update Main.java
    Added some comments to Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println("Hello world 2 !");
+        
+        /* Just Testing to see if it works properly */
 
     }
 
