b1b644af617745fd72e827f79d10586eadb6cd5f
anderson <anderson.mk.ti@gmail.com>
2022-09-29 21:22:25 -0600

    Modified Main.Java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,33 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println("Hello world 2 !");
+
+
+        int var1;
+        var1 = 5;
+
+        int var2;
+        var2 = 6;
+
+        System.out.println("The result is: " + (var1 + var2));
         
         /* Just Testing to see if it works properly */
+        double[] arr = {1,2,3};
 
+        System.out.println("Result: " + sum( arr ));
+
+    }
+
+    public static double sum(double[] args){
+
+        double result;
+
+        result = 0;
+        for (double arg : args) {
+            result += arg;
+        }
+
+        return result;
     }
 
 
